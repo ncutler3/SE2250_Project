4b2195c4fa3eb6462a5cb6fca34ef3b64f93da38
jtan334 <60947320+jtan334@users.noreply.github.com>
2021-02-23 22:36:40 -0500

    Added project file to the repo

---
+++ ProjectVersion0.1/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(5, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(0, 5);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(0, -5);
+        }
+    }
+
+
+}
